e9cd28757a5f2cea8c3fcca6fc0f42edf287c5d5
kaasijo <casvanneerven@icloud.com>
2024-12-28 16:21:38 +0100

    2.0 idk

--- script.js
+++ script.js
@@ -20,20 +20,40 @@ const questions = [
     {
         question: "welk lied past goed bij oud en nieuw",
         answers: [
-            { text: "happy new year", correct: true },
-            { text: "we wish you an merry christmas", correct: false },
-            { text: "psv clublied", correct: false },
-            { text: "wilhelmus", correct: false }
+            { text: "A. happy new year", correct: true },
+            { text: "B. we wish you an merry christmas", correct: false },
+            { text: "C. psv clublied", correct: false },
+            { text: "D. wilhelmus", correct: false }
         ]
     },
     {
         question: "Wat drinken mensen om 12 uur’s nachts met Oudjaar vaak?",
         answers: [
-            { text: "water", correct: false },
-            { text: "bier", correct: false },
-            { text: "champagne", correct: true },
-            { text: "limoncello", correct: false }
+            { text: "A. water", correct: false },
+            { text: "B. bier", correct: false },
+            { text: "C. champagne", correct: true },
+            { text: "D. limoncello", correct: false }
         ]
+    },
+    {
+        question: "op welk plein in new york laten ze om 12 uur met oud en nieuw een bal vallen?",
+        answers: [
+            { text: "A. 5th avenue", correct: false },
+            { text: "B central park", correct: false },
+            { text: "C bij de kerk", correct: false },
+            { text: "D times square", correct: true }
+        ]
+    },
+    {
+        question: "welke dag valt 31 dec in 2024?",
+        answers: [
+            { text: "A. maandag", correct: false },
+            { text: "dinsdag", correct: true },
+            { text: "woensdag", correct: false },
+            { text: "donderdag", correct: false },
+            { text: "vrijdag", correct: false},
+            { text: "zaterdag", correct: false }
+        ] 
     },
     {
         question: "op welk plein in new york laten ze om 12 uur met oud en nieuw een bal vallen?",
@@ -43,17 +63,6 @@ const questions = [
             { text: "bij de kerk", correct: false },
             { text: "times square", correct: true }
         ]
-    },
-    {
-        question: "welke dag valt 31 dec in 2024?",
-        answers: [
-            { text: "maandag", correct: false },
-            { text: "dinsdag", correct: true },
-            { text: "woensdag", correct: false },
-            { text: "donderdag", correct: false },
-            { text: "vrijdag", correct: false},
-            { text: "zaterdag", correct: false }
-        ] 
     },
 ];
 //hallo ik ben cas en dit is van mij
@@ -164,13 +173,16 @@ function updateCurrentTeam() {
 }
 
 function showEndScreen() {
-    questionElement.innerText = 'Quiz afgelopen!   gemaakt door: Cas en Ties';
+    questionElement.innerText = 'Quiz afgelopen!';
     answerButtonsElement.innerHTML = '';
-    nextButton.classList.add('hide');
+    nextButton.textContent = 'Opnieuw'; // Verander de tekst van de knop
+    nextButton.classList.remove('hide');
     scoreElement.innerText = `${team1Input.value || 'Team 1'}: ${team1Score} - ${team2Input.value || 'Team 2'}: ${team2Score} - ${team3Input.value || 'Team 3'}: ${team3Score}`;
     const endMessage = document.createElement('p');
     endMessage.innerText = 'Bedankt voor het spelen!';
     document.getElementById('quiz-container').appendChild(endMessage);
+    quizContainer.classList.add('hide');
+    fireworksVideo.classList.remove('hide');
 }
 
 nextButton.addEventListener('click', () => {
