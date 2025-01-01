85497e30860eff46a84fc7d45641f2b0d1142fc3
hzm74 <hvneerven@protonmail.com>
2025-01-01 12:35:50 +0100

    Extra uitleg toegevoegd aan de javascript file

--- script.js
+++ script.js
@@ -176,17 +176,17 @@ function showQuestion() {
         button.innerText = answer.text; //VERANDER DE TEKST IN DE BUTTON MET DE TEKST VAN HET ANTWOORD VAN DE HUIDIGE VRAAG
         button.classList.add('btn'); //VOEG DE BUTTON TOE AAN CLASS ´btn' CLASS IS STIJL VAN DE BUTTON, ZIE styles.css
         if (answer.correct) { //ALS HET ANTWOORD GOED IS DOE DIT:
-            button.dataset.correct = answer.correct;
+            button.dataset.correct = answer.correct; //TODO VOLGENS MIJ IS DIT DAT HET JUISTE ANTWOORD AAN DE DATASET WORDT TOEGEVOEGD
         }
-        button.addEventListener('click', selectAnswer);
-        answerButtonsElement.appendChild(button);
+        button.addEventListener('click', selectAnswer); //ALS ER OP DE KNOP GEDRUKT (click) WORDT DAN VOER FUNCTIE selectAnswer UIT
+        answerButtonsElement.appendChild(button); // VOEG DE KNOP TOE AAN answerButtonsElement. DAT IS EEN PLEK WAAR DE KNOPPEN KOMEN (ZIE OOK DE HTML FILE)
     });
 }
 
 function resetState() {
     nextButton.classList.add('hide'); //VERBERG DE NEXT BUTTON
-    while (answerButtonsElement.firstChild) { //ZOLANG ER ANTWOORD KNOPPEN ZIJN VERWIJDER DEZE
-        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    while (answerButtonsElement.firstChild) { //ZOLANG ER ANTWOORD-KNOPPEN ZIJN 
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild); //VERWIJDER ALLE KNOPPEN
     }
     answersSelected = 0; //ZET VARIABELE OP 0. DUS GEEN ANTWOORDEN GESELECTEERD MEER. 
 }
